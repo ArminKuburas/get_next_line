52c7c04028091d9bf8315f1c4c3bcb8a0cda016c
Armin Kuburas <akuburass@gmail.com>
2023-12-04 15:24:42 +0200

    Currently still working on the code. Decided to push now just in case.@

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,33 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 23:43:17 by akuburas          #+#    #+#             */
-/*   Updated: 2023/11/30 23:44:29 by akuburas         ###   ########.fr       */
+/*   Updated: 2023/12/04 15:18:20 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char *ft_line_allocator(int fd, char *line_storage)
+{
+	char	*buffer;
+	int		bytes;
+
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+	{
+		free(line_storage);
+		return (NULL);
+	}
+	bytes = 1;
+	while(strchr)
+}
+
 char	*get_next_line(int fd)
 {
-	
+	char		*line;
+	static char	*line_storage;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	line_storage = ft_line_allocator(fd, line_storage);
 }
---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 11:26:06 by akuburas          #+#    #+#             */
+/*   Updated: 2023/12/04 11:26:07 by akuburas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+#endif
