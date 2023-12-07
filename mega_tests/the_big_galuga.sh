48a12d16129f9ec847a5ee70312f5390446a36d9
Armin Kuburas <akuburass@gmail.com>
2023-12-07 16:40:36 +0200

    First try at making the bash script

--- mega_tests/the_big_galuga.sh
+++ mega_tests/the_big_galuga.sh
@@ -4,3 +4,18 @@
 
 CC=cc
 CFLAGS="-WAll -Wextra -Werror"
+
+TEST_FILES("e_main.c" "lm_main.c" "ls_main.c" "lt_main.c" "ss_main.c" "st_main.c" "sm_main.c" "w_main.c" "n_main.c")
+
+for test_file in "${TEST_FILES[@]}"; do
+	test_name=$(basename "$test_file" .c)
+	$CC $CFLAGS -o "$test_name" get_next_line.c get_next_line_utils.c "$test_file"
+	if [ $? -eq 0 ]; then
+		echo "Running test: $test_name"
+		./"$test_name"
+	else
+		echo "Error compiling test: $test_name"
+	fi
+
+	rm -f "$test_name
+done
