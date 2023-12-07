371b343b69042fde92548ad1a6f7c0e33e361966
Armin Kuburas <akuburass@gmail.com>
2023-12-07 16:55:49 +0200

    Have a working bash script

--- mega_tests/the_big_galuga.sh
+++ mega_tests/the_big_galuga.sh
@@ -3,13 +3,13 @@
 #Lets gooooo!
 
 CC=cc
-CFLAGS="-WAll -Wextra -Werror"
+CFLAGS="-Wall -Wextra -Werror"
 
-TEST_FILES("e_main.c" "lm_main.c" "ls_main.c" "lt_main.c" "ss_main.c" "st_main.c" "sm_main.c" "w_main.c" "n_main.c")
+TEST_FILES=("e_main.c" "lm_main.c" "ls_main.c" "lt_main.c" "ss_main.c" "st_main.c" "sm_main.c" "w_main.c" "n_main.c")
 
 for test_file in "${TEST_FILES[@]}"; do
 	test_name=$(basename "$test_file" .c)
-	$CC $CFLAGS -o "$test_name" get_next_line.c get_next_line_utils.c "$test_file"
+	$CC $CFLAGS  -D BUFFER_SIZE=10000000 -o "$test_name" get_next_line.c get_next_line_utils.c "$test_file"
 	if [ $? -eq 0 ]; then
 		echo "Running test: $test_name"
 		./"$test_name"
@@ -17,5 +17,5 @@ for test_file in "${TEST_FILES[@]}"; do
 		echo "Error compiling test: $test_name"
 	fi
 
-	rm -f "$test_name
+	rm -f "$test_name"
 done
